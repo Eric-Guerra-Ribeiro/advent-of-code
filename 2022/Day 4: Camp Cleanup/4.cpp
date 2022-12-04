5990a4bd961577e257607fa1ea4695607b24b6bb
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2022-12-04 09:41:58 -0300

    solving day4.2 2022

--- 2022/Day 4: Camp Cleanup/4.cpp
+++ 2022/Day 4: Camp Cleanup/4.cpp
@@ -10,18 +10,23 @@ int main(){
     int min1st, max1st;
     int min2nd, max2nd;
 
-    int overlaps = 0;
+    int overlapsFully = 0;
+    int overlapsAtAll = 0;
 
     input.open("input.txt", ios::in);
 
     while (input >> min1st >> del >> max1st >> del >> min2nd >> del >> max2nd) {
-        if ((min1st <= min2nd && max2nd <= max1st)
-            || (min2nd <= min1st && max1st <= max2nd)) {
-            ++overlaps;
+        if (    (min1st <= min2nd && max2nd <= max1st)
+             || (min2nd <= min1st && max1st <= max2nd)) {
+            ++overlapsFully;
+        }
+        if  (min2nd <= max1st && min1st <= max2nd) {
+            ++overlapsAtAll;
         }
     }
 
-    cout << overlaps << "\n";
+    cout << "Overlaps fully: " << overlapsFully << "\n";
+    cout << "Overlaps at all: " << overlapsAtAll << "\n";
 
     input.close();
     return 0;
