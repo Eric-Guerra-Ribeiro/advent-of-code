413757bea423c2608980be85265165133fdc734a
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2024-12-03 13:22:54 +0100

    fix deleting wrong file

--- 2024/Day 2 - Red-Nosed Reports/2.py
+++ 2024/Day 2 - Red-Nosed Reports/2.py
@@ -3,12 +3,39 @@ from pathlib import Path
 import numpy as np
 
 def read_input(path: Path) -> list[np.ndarray]:
-    pass
+    with open(path) as file:
+        lines = file.readlines()
+        return [np.array(line.split(), dtype=int) for line in lines]
 
+
+def is_safe(level: np.ndarray) -> bool:
+    if len(level) <=1 :
+        return True
+    diff = level[1:] - level[:-1]
+    abs_diff = np.abs(diff)
+    # Safe level must differ by at most 3
+    if np.max(abs_diff) > 3:
+        return False
+    # Safe level must differ by at least 1
+    if np.min(abs_diff) < 1:
+        return False
+    # Safe level must be increasing or decreasing
+    num_increasing = np.sum(diff > 0)
+    if num_increasing == 0 or num_increasing == len(diff):
+        return True
+    return False
 
 
 def solve_part1(input: list[np.ndarray]) -> int:
-    pass
+    safe = sum(is_safe(level) for level in input)
+    return safe
+
+
+def is_dampened_safe(level: np.ndarray) -> bool:
+    for i in range(len(level)):
+        if is_safe(np.delete(level, i)):
+            return True
+    return False
 
 
 def solve_part2(input: list[int]) ->int:
